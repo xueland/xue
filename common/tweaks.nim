a1d6fbd7fef8123b04abf777e7f97ea2ab14b064
Hein Thant Maung Maung <me@heinthanth.com>
2021-08-20 11:11:58 +0630

    add CLI parser

--- common/tweaks.nim
+++ common/tweaks.nim
@@ -3,8 +3,14 @@ import strformat, strutils
 const
     NoNewLinePadding* {.booldefine.} = false ## whether new-line padding should be inserted when `printPaddedLine` is called
 
-    XUE_VERSION_MAJOR* {.intdefine.} = 1
-    XUE_VERSION_MINOR* {.intdefine.} = 0
-    XUE_VERSION_PATCH* {.intdefine.} = 0
+    XUE_VERSION_MAJOR* {.intdefine.} = 1 ## xue major version number
+    XUE_VERSION_MINOR* {.intdefine.} = 0 ## xue minor version number
+    XUE_VERSION_PATCH* {.intdefine.} = 0 ## xue patch version number
     XUE_VERSION_BUILD* = fmt"{CompileDate}{CompileTime}".multiReplace(("-", ""), (":", ""))
+        ## build number is generated basded on `CompileDate` and `CompileTime`
     XUE_VERSION_STRING* = fmt"{XUE_VERSION_MAJOR}.{XUE_VERSION_MINOR}.{XUE_VERSION_PATCH}"
+        ## build number is generated basded major, minor, patch version.
+
+    EXIT_STATUS_SUCCESS* = 0        ## operation is succeeded
+    EXIT_STATUS_FAILURE* = 1        ## operation is failed with some error
+    EXIT_STATUS_WRONG_USAGE* = 2    ## operation is failed due to invalid CLI usage, etc.
--- xue.nim
+++ xue.nim
@@ -1,5 +1,6 @@
 import common/[ioutils,tweaks]
-import strutils
+import strutils, parseopt
+import terminal
 
 proc printVersion(shouldExit: bool = true) =
     when defined(release):
@@ -9,5 +10,60 @@ proc printVersion(shouldExit: bool = true) =
     printPaddedLine(versionString, XUE_VERSION_STRING, XUE_VERSION_BUILD, hostOS, hostCPU)
     if shouldExit: quit()
 
+proc printHelp() =
+    printVersion(false)
+    const helpString = [
+        "SYNOPSIS:\n",
+        "   xue [options]... [script]...\n",
+        "OPTIONS:\n",
+        "    -r, --run          run the code pass with -r option\n",
+        "    -v, --version      print Xue version and others.",
+        "    -h, --help         print this help message like usage, options, etc.\n",
+        "REPORTING:\n",
+        "    https://github.com/xueland/xue/issues. Contributions are also welcome!\n"
+    ].join("\n")
+    printLine(helpString); quit(EXIT_STATUS_FAILURE)
+
 when isMainModule:
-    printVersion()
+    var
+        inputScript: string ## xue script to interpret
+        runnableCode: string ## code that's passed with -r option
+
+    for kind, option, value in getopt(shortNoVal = {'h', 'v'},
+            longNoVal = @["help", "version"]):
+        case kind
+        of cmdEnd:
+            printPaddedLine("Oops, something went wrong while parsing CLI arguments!")
+            quit(EXIT_STATUS_FAILURE)
+        of cmdLongOption:
+            case option
+            of "help": printHelp()
+            of "version": printVersion()
+            of "run":
+                runnableCode = value
+            of "": discard
+            else:
+                printPaddedLine("Oops, --%s is not a valid option. See 'xue --help'.", option)
+                quit(EXIT_STATUS_WRONG_USAGE)
+        of cmdShortOption:
+            case option
+            of "h": printHelp()
+            of "v": printVersion()
+            of "r":
+                runnableCode = value
+            of "":
+                inputScript = "-"
+            else:
+                printPaddedLine("Oops, -%s is not a valid option. See 'xue --help'.", option)
+                quit(EXIT_STATUS_WRONG_USAGE)
+        of cmdArgument:
+            inputScript = option
+
+    if runnableCode != "":
+        printPaddedLine("running from code passed with -r.")
+    elif inputScript != "":
+        printPaddedLine("running from file: '%s'.", inputScript)
+    elif isatty(stdin):
+        printPaddedLine("running from REPL.")
+    else:
+        printPaddedLine("running from stdin.")
